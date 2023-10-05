30877feceef409d626962729f70806763fa9f231
godwinn22 <godwinohazulike@gmail.com>
2023-10-06 00:41:19 +0100

    Made all have a format specifier of lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)", sizeof(char));
-	printf("Size of a char: %zu byte(s)", sizeof(int));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of a char: %lu byte(s)", sizeof(int));
 	printf("Size of a char: %lu byte(s)", sizeof(long int));
 	printf("Size of a char: %lu byte(s)", sizeof(long long int));
-	printf("Size of a char: %zu byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)", sizeof(float));
 	return (0);
 }
