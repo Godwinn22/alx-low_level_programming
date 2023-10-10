06b386b561c1747af2715d86a2a3e30aa876c71e
godwinn22 <godwinohazulike@gmail.com>
2023-10-11 00:49:13 +0100

    Edited to betty standard

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 
 /**
  * _islower - Entry point
+ * @c: an integer parameter.
  *
  * Return: Always 0 (success)
 */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
