29cd8f8ece1ac13dcc4dcac8ddfbdafc1bd3806c
godwinn22 <godwinohazulike@gmail.com>
2023-10-11 16:56:56 +0100

    edited

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,10 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (b = 1; b <= 9; b++)
 		{
 			multiplier = a * b;
 			if ((multiplier / 10) > 0)
