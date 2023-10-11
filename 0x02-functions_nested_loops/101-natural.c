7becab71b773050aa2fdd96caddf07578273903b
godwinn22 <godwinohazulike@gmail.com>
2023-10-11 23:29:41 +0100

    I solved a problem that says "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line. You are allowed to use the standard library"

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,9 +2,12 @@
 
 /**
  * main - Entry point
- * Description: If we list all the natural numbers 
- * below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded),
- *  followed by a new line. You are allowed to use the standard library
+ * Description: If we list all the natural numbers
+ * below 10 that are multiples of 3 or 5, we get
+ * 3, 5, 6 and 9. The sum of these multiples is
+ * 23. Write a program that computes and prints
+ * the sum of all the multiples of 3 or 5 below 1024 (excluded),
+ * followed by a new line. You are allowed to use the standard library
  * Return: Always 0 (success)
 */
 int main(void)
@@ -20,4 +23,5 @@ int main(void)
 		}
 	}
 	printf("%d\n", sum_of_multiples);
+	return (0);
 }
