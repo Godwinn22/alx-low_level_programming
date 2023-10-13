3c0a3c4575973a8fdb217dd14af4bc0f2fde73a8
godwinn22 <godwinohazulike@gmail.com>
2023-10-13 19:27:21 +0100

    Solved task 9-Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,21 +22,21 @@ int main(void)
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
+		}
+		if (i < 100)
+		{
 			printf(" ");
 		}
 	}
