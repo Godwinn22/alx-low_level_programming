6bf403917ad54fed8e79069f114a1ac526597b2f
godwinn22 <godwinohazulike@gmail.com>
2023-10-13 18:35:00 +0100

    I feel so frustrated now

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,19 +8,18 @@
 */
 void print_diagonal(int n)
 {
+	int a;
+	int b;
+
 	if (n <= 0)
 		_putchar('\n');
-	else
+	for (a = 0; a < n; a++)
 	{
-		int a;
-		int b;
-
-		for (a = 0; a < n; a++)
+		for (b = 0; b < a; b++)
 		{
-			for (b = 0; b <= a; b++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
