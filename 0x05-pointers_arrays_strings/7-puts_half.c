bd4bb226499d82c2af91e49e0b220273eb5688d1
godwinn22 <godwinohazulike@gmail.com>
2023-10-17 12:09:21 +0100

    Solved task "7. Winning is only half of it. Having fun is the other half"

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half -a function that prints half of a string, followed by a new line.
+ * @str: the variable that contains our string
+ *
+ * Return: returning the value of count.
+*/
+void puts_half(char *str)
+{
+	int i, counter, half_length;
+
+	counter = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	if (counter % 2 == 0)
+	{
+		half_length = counter / 2;
+		for (i = half_length; i < counter; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		half_length = (counter - 1) / 2;
+		for (i = half_length; i < counter; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
