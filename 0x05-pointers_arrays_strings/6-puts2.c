ce070d677c65d0e071d264122518c8f3a5e8de19
godwinn22 <godwinohazulike@gmail.com>
2023-10-17 09:58:39 +0100

    Solved task "6. Half the lies they tell about me aren't true"

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: the variable that contains our string
+ *
+ * Return: returning the value of count.
+*/
+void puts2(char *str)
+{
+	int i, counter;
+
+	counter = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	for (i = 0; i <= counter; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
