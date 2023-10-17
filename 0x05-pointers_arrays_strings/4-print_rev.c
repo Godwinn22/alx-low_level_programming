268ada1004dbab9f79cbb04099cf6272a42963bc
godwinn22 <godwinohazulike@gmail.com>
2023-10-17 09:11:50 +0100

    Solved task "4. I can only go one way. I've not got a reverse gear"

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev -a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: the variable that contains our string
+ *
+ * Return: returning the value of count.
+*/
+void print_rev(char *s)
+{
+	int i, counter;
+
+	counter = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* code to get the length first */
+		counter++;
+	}
+	for (i = counter - 1; s[i] >= 0; i--)
+	{
+		/* printing the characters in reverse */
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
