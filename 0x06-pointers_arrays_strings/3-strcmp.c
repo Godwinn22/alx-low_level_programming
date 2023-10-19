34a37dd1220f4ab61c8546f4c694aff025c7b769
godwinn22 <godwinohazulike@gmail.com>
2023-10-19 19:08:54 +0100

    Solved task "3. strcmp"

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,18 +15,14 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] < s2[i])
+			if (s2[i] > s1[i])
 			{
 				return (s1[i] - s2[i]);
 			}
-			else if (s2[i] < s1[i])
+			else if (s1[1] > s2[i])
 			{
 				return (s1[i] - s2[i]);
 			}
-		}
-		else
-		{
-			return (0);
 		}
 	}
 	return (0);
