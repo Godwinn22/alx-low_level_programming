7a147d41f92da83d3b150eb55c3cabf495c44f64
godwinn22 <godwinohazulike@gmail.com>
2023-11-01 03:31:53 +0100

    Solved task "3. If you even dream of beating me you'd better wake up and apologize"

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * **alloc_grid - a function that returns
+ * a pointer to a 2 dimensional array of integers.
+ * @width: the columns of the 2d array
+ * @height: the rows of the 2d array
+ * Return: return NULL on failure
+*/
+int **alloc_grid(int width, int height)
+{
+	int **result;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	result = malloc(height * sizeof(int *));
+
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		result[i] = malloc(width * sizeof(int *));
+		if (result[i] == NULL)
+		{
+			free(result);
+			for (j = 0; j <= height; j++)
+			{
+				free(result[j]);
+			}
+			return (NULL);
+		}
+		else
+		{
+			for (j = 0; j < width; j++)
+			{
+				result[i][j] = 0;
+			}
+		}
+	}
+	return (result);
+}
