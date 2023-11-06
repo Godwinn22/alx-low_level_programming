503afaa6f0345c5f5c2215be4ff938abc56354d8
godwinn22 <godwinohazulike@gmail.com>
2023-11-06 21:26:05 +0100

    SOlved 0. Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# This is the directory for my ALX "0x0E-structures_typedef" project.
+
+Join me!!!.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog structure
+ * @name: the name property
+ * @age: the age property
+ * @owner: the owner property
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
