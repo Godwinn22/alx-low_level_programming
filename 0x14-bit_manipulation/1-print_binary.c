8b285c949ce035ffd1107d955f21ee4d7d4e839b
godwinn22 <godwinohazulike@gmail.com>
2023-11-24 19:50:26 +0100

    Solved task 1. 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary representation of a number.
+ * @n: pointer to a character array or string that represents a binary number
+ *
+ * Description: You are not allowed to use arrays
+ * You are not allowed to use malloc
+ * You are not allowed to use the % or / operators
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 0;
+	while (n > 0)
+	{
+		if ((n & 1))
+			printf("1");
+		else
+			printf("0");
+		n = n >> 1;
+		i++;
+	}
+	printf("\n");
+}
