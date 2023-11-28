bd94243b278d0be534c5d3cb49f5f2a48e10fde6
godwinn22 <godwinohazulike@gmail.com>
2023-11-28 02:50:42 +0100

    Solved task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,6 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename,  O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		perror("Error opening file");
 		return (-1);
 	}
 
@@ -32,11 +31,9 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (output == -1)
 	{
-		perror("Error writing to file");
 		return (-1);
 	}
 
 	close(fd);
-	printf("%s created successfully!\n", filename);
 	return (1);
 }
