78c3d33fe6254446d3df9937485485b409ecaa4a
godwinn22 <godwinohazulike@gmail.com>
2023-11-28 02:43:48 +0100

    Solved task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int output = 1, count = 0;
 
-	if (filename == NULL || strlen(filename) < 3)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
@@ -27,13 +27,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[count])
-			count++;
+		count++;
 		output = write(fd, text_content, count);
 	}
 	if (output == -1)
 	{
 		perror("Error writing to file");
-		close(fd);
 		return (-1);
 	}
 
