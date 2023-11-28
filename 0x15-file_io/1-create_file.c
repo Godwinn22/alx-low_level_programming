41afd792736524d03a7afc0ee41b5ed63f05f4cd
godwinn22 <godwinohazulike@gmail.com>
2023-11-28 02:48:20 +0100

    Solved task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,12 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int output = 1, count = 0;
 
-	if (filename == NULL)
+	if (!filename)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename,  O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		perror("Error opening file");
